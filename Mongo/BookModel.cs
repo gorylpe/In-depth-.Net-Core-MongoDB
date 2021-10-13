93de3eaae0ced63755bd624de26e73612a4348e5
Piotr Przestrzelski <gorylp71@gmail.com>
2021-10-13 22:48:00 +0200

    Idek działa

--- Mongo/BookModel.cs
+++ Mongo/BookModel.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Mongo
 {
 	public class BookModel
 	{
-		public ObjectId Idek        { get; set; }
-		public string   Title       { get; set; }
-		public string   Author      { get; set; }
-		public DateTime ReleaseDate { get; set; }
+		[BsonId] public ObjectId Idek        { get; set; }
+		public          string   Title       { get; set; }
+		public          string   Author      { get; set; }
+		public          DateTime ReleaseDate { get; set; }
 
 		public override string ToString() => $"{Idek.ToString()} - {Title} - {Author} - {ReleaseDate.Year}";
 	}
