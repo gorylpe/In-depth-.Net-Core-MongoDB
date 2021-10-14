41d6979f55a2520eede40226b985deb684b95de8
Piotr Przestrzelski <gorylp71@gmail.com>
2021-10-14 15:01:34 +0200

    Conventions added, old discriminators not working

--- Mongo/BookModelSetup.cs
+++ Mongo/BookModelSetup.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Mongo.Reviews;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
@@ -12,46 +14,46 @@ namespace Mongo
 	{
 		public static void Setup()
 		{
+			var allConventionPack = new ConventionPack
+			{
+				new CamelCaseElementNameConvention(),
+				new StringIdStoredAsObjectIdConvention()
+			};
+			ConventionRegistry.Register("All", allConventionPack, _ => true);
+			
+			var reviewType = typeof(IReview);
+			var reviewConventionPack = new ConventionPack
+			{
+				new ReviewDiscriminatorConvention()
+			};
+			ConventionRegistry.Register("Review", reviewConventionPack, type =>
+			{
+				var contains = type.GetInterfaces().Contains(reviewType);
+				return contains;
+			});
+			
 			BsonClassMap.RegisterClassMap<BookModel>(cm =>
 			{
-				cm.MapIdProperty(x => x.Idek)
-					.SetSerializer(new StringSerializer(BsonType.ObjectId))
-					.SetIdGenerator(StringObjectIdGenerator.Instance);
-				cm.MapProperty(x => x.Title)
-					.SetElementName("title");
-				cm.MapProperty(x => x.Author)
-					.SetElementName("author")
+				cm.MapIdMember(x => x.Idek);
+				cm.AutoMap();
+				cm.GetMemberMap(x => x.Author)
 					.SetDefaultValue(BookModel.DefaultAuthor)
 					.SetIgnoreIfDefault(true)
 					.SetSerializer(new AuthorStringSerializer());
-				cm.MapProperty(x => x.ReleaseDate)
-					.SetElementName("releaseDate")
+				cm.GetMemberMap(x => x.ReleaseDate)
 					.SetSerializer(new DateTimeSerializer(true));
-				cm.MapProperty(x => x.Type)
-					.SetElementName("type")
+				cm.GetMemberMap(x => x.Type)
 					.SetSerializer(new EnumSerializer<BookType>(BsonType.String));
-				cm.MapProperty(x => x.Reviews)
-					.SetElementName("reviews");
 
 				cm.SetIgnoreExtraElements(true);
 			});
 
-			BsonClassMap.RegisterClassMap<SimpleReview>(cm =>
-			{
-				cm.MapProperty(x => x.Overall)
-					.SetElementName("overall");
-			});
-			BsonClassMap.RegisterClassMap<ExpertReview>(cm =>
-			{
-				cm.MapProperty(x => x.Overall)
-					.SetElementName("overall");
-				cm.MapProperty(x => x.AdditionalWord)
-					.SetElementName("additionalWord");
-			});
+			BsonClassMap.RegisterClassMap<SimpleReview>();
+			BsonClassMap.RegisterClassMap<ExpertReview>();
 			BsonClassMap.RegisterClassMap<GradeReview>(cm =>
 			{
-				cm.MapProperty(x => x.Grade)
-					.SetElementName("grade")
+				cm.AutoMap();
+				cm.GetMemberMap(x => x.Grade)
 					.SetSerializer(new EnumSerializer<Grade>(BsonType.String));
 			});
 			BsonSerializer.RegisterDiscriminatorConvention(typeof(IReview), StandardDiscriminatorConvention.Scalar);
---
+++ Mongo/ReviewDiscriminatorConvention.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace Mongo
+{
+	public class ReviewDiscriminatorConvention : ConventionBase, IClassMapConvention
+	{
+		private const string SuffixToRemove = "Review";
+
+		public void Apply(BsonClassMap classMap)
+		{
+			var type = classMap.ClassType;
+			var discriminator = type.Name;
+			if (discriminator.EndsWith(SuffixToRemove))
+				discriminator = discriminator.Substring(0, discriminator.Length - SuffixToRemove.Length);
+			classMap.SetDiscriminator(discriminator);
+		}
+	}
+}
